8d0f6abbcb3547b04e521dd48218e6d827f8010c
unknown <freesa033@gmail.com>
2025-09-13 22:31:21 -0500

    se resolvio un algoritmo

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+print("Tablas")
+
+numero = int(input("Ingrese un número: "))
+contador = 0
+for i in range(0, numero):
+    contador += i + 1
+print(f"La suma de los números asta {numero} es: {contador}")
+
+frutas = ["manzana", "pera", "uva"]
+for fruta in frutas:
+    print(fruta)
+
+palabra = 'Hola'
+print(palabra[0], palabra [3])
+
